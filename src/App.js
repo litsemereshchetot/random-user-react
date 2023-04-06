184122ef0c93b2d2cecf3b2a3caa216391b2b7d5
litsemereshchetot <95826876+litsemereshchetot@users.noreply.github.com>
2023-04-06 16:20:32 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  async function fetchUser() {
+    const response = await axios.get('http://127.0.0.1:8080/users');
+    const data = response.data.results[0];
+    setUser(data);
+  }
+
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Button onClick={fetchUser}>Get Random User</Button>
+        </Col>
+      </Row>
+      {user && (
+        <Row className="mt-5">
+          <Col md={{ span: 6, offset: 3 }}>
+            <Card>
+              <Card.Img variant="top" src={user.picture.large} />
+              <Card.Body>
+                <Card.Title>{`${user.name.title} ${user.name.first} ${user.name.last}`}</Card.Title>
+                <Card.Text>
+                  {`Age: ${user.dob.age}`}
+                  <br />
+                  {`Email: ${user.email}`}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </Container>
+  );
+}
+
+export default App;
